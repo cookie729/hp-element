b1558a388e4ec398a3ead4007680fd26baffc557
*fQD1sd97jX <sakai.kouki@ring-tech.jp>
2023-11-23 23:32:57 +0900

    feat: hamburger Jquery

---
+++ js/hamburger.js
@@ -0,0 +1,31 @@
+'use strict';
+
+{
+  // Hamburger-menu --------------
+  const open = document.getElementById('open');
+  const overlay = document.querySelector('.overlay');
+  const close = document.getElementById('close');
+  open.addEventListener('click', () => {
+    overlay.classList.add('show');
+    open.classList.add('hide');
+  });
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+  overlay.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
+  });
+}
+
+// Jquery;
+$(document).ready(function () {
+  $('#j-open').click(function () {
+    $('.j-overlay').fadeIn();
+  });
+
+  $('#j-close').click(function () {
+    $('.j-overlay').fadeOut();
+  });
+});
---
+++ js/wrapper.js
@@ -0,0 +1,27 @@
+'use strict';
+
+{
+  // Wrapper animation-----------
+  function callback(entries, obs) {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  const options = {
+    threshold: 0.2,
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  const targets = document.querySelectorAll('.wrapper, .text');
+
+  targets.forEach((target) => {
+    observer.observe(target);
+  });
+}
