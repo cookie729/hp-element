77ce605dfca1d754f5765fbfb124e2ed306daea8
*fQD1sd97jX <sakai.kouki@ring-tech.jp>
2023-11-25 00:11:10 +0900

    fix: hamburger

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,23 +1,23 @@
 'use strict';
 
-{
-  // Hamburger-menu --------------
-  const open = document.getElementById('open');
-  const overlay = document.querySelector('.overlay');
-  const close = document.getElementById('close');
-  open.addEventListener('click', () => {
-    overlay.classList.add('show');
-    open.classList.add('hide');
-  });
-  close.addEventListener('click', () => {
-    overlay.classList.remove('show');
-    open.classList.remove('hide');
-  });
-  overlay.addEventListener('click', () => {
-    overlay.classList.remove('show');
-    open.classList.remove('hide');
-  });
-}
+// {
+//   // Hamburger-menu --------------
+//   const open = document.getElementById('open');
+//   const overlay = document.querySelector('.overlay');
+//   const close = document.getElementById('close');
+//   open.addEventListener('click', () => {
+//     overlay.classList.add('show');
+//     open.classList.add('hide');
+//   });
+//   close.addEventListener('click', () => {
+//     overlay.classList.remove('show');
+//     open.classList.remove('hide');
+//   });
+//   overlay.addEventListener('click', () => {
+//     overlay.classList.remove('show');
+//     open.classList.remove('hide');
+//   });
+// }
 
 // Jquery;
 $(document).ready(function () {
