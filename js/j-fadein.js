40bcd962c12c526c427d1a3920f9c723bc6b3851
*fQD1sd97jX <sakai.kouki@ring-tech.jp>
2023-12-03 23:12:22 +0900

    feat: fade adjust

--- js/j-fadein.js
+++ js/j-fadein.js
@@ -1,29 +1,29 @@
 'use strict';
 
-{
-  // 要素が画面の表示範囲に入るときに表示される -----------
-  function callback(entries, obs) {
-    entries.forEach((entry) => {
-      if (!entry.isIntersecting) {
-        return;
-      }
-      entry.target.classList.add('appear');
-      obs.unobserve(entry.target);
-    });
-  }
+// {
+//   // 要素が画面の表示範囲に入るときに表示される -----------
+//   function callback(entries, obs) {
+//     entries.forEach((entry) => {
+//       if (!entry.isIntersecting) {
+//         return;
+//       }
+//       entry.target.classList.add('appear');
+//       obs.unobserve(entry.target);
+//     });
+//   }
 
-  const options = {
-    threshold: 0.2,
-  };
+//   const options = {
+//     threshold: 0.2,
+//   };
 
-  const observer = new IntersectionObserver(callback, options);
+//   const observer = new IntersectionObserver(callback, options);
 
-  const targets = document.querySelectorAll('.wrapper, .fade-wrap');
+//   const targets = document.querySelectorAll('.wrapper, .fade-wrap');
 
-  targets.forEach((target) => {
-    observer.observe(target);
-  });
-}
+//   targets.forEach((target) => {
+//     observer.observe(target);
+//   });
+// }
 
 $(function () {
   $(window).scroll(function () {
