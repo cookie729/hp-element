a3d30ce2f7d820e4a29c5a83291e7287c8140679
*fQD1sd97jX <sakai.kouki@ring-tech.jp>
2023-12-04 23:11:09 +0900

    feat: complete fade

--- js/j-fadein.js
+++ js/j-fadein.js
@@ -1,38 +1,36 @@
 'use strict';
 
-// {
-//   // 要素が画面の表示範囲に入るときに表示される -----------
-//   function callback(entries, obs) {
-//     entries.forEach((entry) => {
-//       if (!entry.isIntersecting) {
-//         return;
+// $(function () {
+//   $(window).scroll(function () {
+//     $('.fade').each(function () {
+//       var pos = $(this).offset().top;
+//       var scroll = $(window).scrollTop();
+//       var windowHeight = $(window).height();
+//       if (scroll > pos - windowHeight + 100) {
+//         $(this).addClass('scroll');
 //       }
-//       entry.target.classList.add('appear');
-//       obs.unobserve(entry.target);
 //     });
-//   }
-
-//   const options = {
-//     threshold: 0.2,
-//   };
-
-//   const observer = new IntersectionObserver(callback, options);
-
-//   const targets = document.querySelectorAll('.wrapper, .fade-wrap');
-
-//   targets.forEach((target) => {
-//     observer.observe(target);
 //   });
-// }
-
+// });
 $(function () {
+  $('.fade').each(function () {
+    var pos = $(this).offset().top;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll > pos - windowHeight + 100) {
+      $(this).addClass('scroll');
+    }
+  });
+
   $(window).scroll(function () {
-    $('.js-fade').each(function () {
+    $('.fade').each(function () {
       var pos = $(this).offset().top;
       var scroll = $(window).scrollTop();
       var windowHeight = $(window).height();
       if (scroll > pos - windowHeight + 100) {
         $(this).addClass('scroll');
+      } else {
+        $(this).removeClass('scroll'); // 追加: スクロールから外れたら非表示にする
       }
     });
   });
