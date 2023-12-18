4b9dfb46f4d6aa12e82f12d0137ee3850fe5dc21
*fQD1sd97jX <sakai.kouki@ring-tech.jp>
2023-12-18 23:26:59 +0900

    feat: create hamburger-slide

--- js/j-hamburger-slide.js
+++ js/j-hamburger-slide.js
@@ -1,14 +1,11 @@
 
 'use strict'
 
-
-new LuminousGallery(document.querySelectorAll(".gallery-lightbox"));
-
-const category = document.querySelector('.category-menu');
-const nav = document.querySelector('.category-nav');
-category.addEventListener('click', () => {
-  nav.classList.toggle('open-menu');
-});
+// const category = document.querySelector('.category-menu');
+// const nav = document.querySelector('.category-nav');
+// category.addEventListener('click', () => {
+//   nav.classList.toggle('open-menu');
+// });
 
 const btn = document.querySelector('.btn-menu');
 const mobile_nav = document.querySelector('.mobile-main-nav');
