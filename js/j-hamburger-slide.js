64fa5096f4b471eb45a36d576b2482bcbb963751
*fQD1sd97jX <sakai.kouki@ring-tech.jp>
2023-12-19 23:12:08 +0900

    feat: hamburger adjustment

--- js/j-hamburger-slide.js
+++ js/j-hamburger-slide.js
@@ -8,13 +8,13 @@
 // });
 
 const btn = document.querySelector('.btn-menu');
-const mobile_nav = document.querySelector('.mobile-main-nav');
+const mobile_nav = document.querySelector('.main-nav');
 btn.addEventListener('click', () => {
   mobile_nav.classList.toggle('open-menu');
 });
 
-const mobile_category = document.querySelector('.mobile-category-menu');
-const mobile_category_nav = document.querySelector('.mobile-category-nav');
+const mobile_category = document.querySelector('.category-menu');
+const mobile_category_nav = document.querySelector('.category-nav');
 mobile_category.addEventListener('click', () => {
   mobile_category_nav.classList.toggle('open-menu');
 });
